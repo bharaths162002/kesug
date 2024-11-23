ef6c9f5231005a7c049e869b4cc22239537fdec9
bharaths162002 <151670967+bharaths162002@users.noreply.github.com>
2024-11-23 20:30:41 +0530

    Delete jobBoard/jobBoard directory

--- jobBoard/jobBoard/src/main/java/com/jobBoard/jobBoard/Repository/JobRegistrationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jobBoard.jobBoard.Repository;
-
-import com.jobBoard.jobBoard.Entity.JobRegistration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface JobRegistrationRepository extends JpaRepository<JobRegistration,Integer> {
-}
